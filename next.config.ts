981ad6ae57dd299c3cdc38531aab8530bb784b74
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2025-07-29 23:32:18 +0200

    Prevent CSS from blocking page loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
+    // At the time of writing, Next.js doesn't inline critical-path CSS and
+    // since there's so little CSS, we can inline it all.
+    inlineCss: true,
     typedRoutes: true,
   },
   images: {
