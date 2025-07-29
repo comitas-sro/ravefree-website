48ce955a43183dabd67098f9904660fea605a0b9
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2025-07-29 15:07:49 +0200

    Implement Peter's template

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  images: {
+    // Optimization is unsupported for `output: 'export'`.
+    unoptimized: true,
+  },
   output: 'export',
   trailingSlash: true,
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,41 @@
+import type { Metadata } from "next";
 import Image from "next/image";
+
+import mobile_banner from "./mobile-banner.png";
+import pc_banner from "./pc-banner.png";
 import styles from "./page.module.css";
+
+export const metadata: Metadata = {
+  title: "Kontaktný formulár",
+};
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <Image
+        className={`${styles.banner} ${styles.mobileBanner}`}
+        src={mobile_banner}
+        alt="Banner"
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Image
+        className={`${styles.banner} ${styles.pcBanner}`}
+        src={pc_banner}
+        alt="Banner"
+        />
+      <div className={styles.formContainer}>
+        <h1>Registrácia pre vstup zdarma</h1>
+        <iframe
+          className={styles.form}
+          width="640px"
+          height="480px"
+          src="https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=egtGmlDYeUKxxu4-_TVj3xOnwCQn8N5PhN8vGOobWQ5UOFdaUTM3N09ROEtTUkFHQktBS0ZMSVlFQy4u&embed=true"
+          frameBorder="0"
+          marginWidth={0}
+          marginHeight={0}
+          allowFullScreen
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </iframe>
+      </div>
+    </>
   );
 }
