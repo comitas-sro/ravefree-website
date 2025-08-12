51aa47089ad94a098c9be3c9ab39e5be2aaf12cd
Samuel Mitas <samomitas@gmail.com>
2025-08-12 21:15:21 +0200

    Rework banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import Image from 'next-export-optimize-images/image';
 
-import mobile_banner from './mobile-banner.webp';
-import pc_banner from './pc-banner.webp';
+import video_banner_fallback from './video-banner-fallback.webp';
+import banner from './banner.webp';
+import genesi from './genesi.webp';
 import { SignUpForm } from './components/common/SignUpForm';
 
 export const metadata: Metadata = {
@@ -11,22 +12,41 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className='bg-linear-to-t from-[#20003A] to-zink-900 p-20'>
+    <div className="bg-linear-to-t from-[#20003A] to-black py-20">
       <Image
-        className='md:hidden'
+        className="max-w-sm m-auto px-14"
         fetchPriority="high"
         layout="responsive"
         loading="eager"
-        src={mobile_banner}
-        alt="Banner"
+        src={banner}
+        alt="RAVE FREE"
       />
+      <video
+        autoPlay
+        playsInline // Required for iOS to play without user interaction
+        loop
+        muted
+        width={600}
+        height={600}
+        className="m-auto"
+      >
+        <source src="/video-banner.webm" type="video/webm" />
+        <Image
+          className="max-w-sm m-auto px-14"
+          fetchPriority="high"
+          layout="responsive"
+          loading="eager"
+          src={video_banner_fallback}
+          alt="RF"
+        />
+      </video>
       <Image
-        className='hidden md:block'
+        className='max-w-sm m-auto px-16'
         fetchPriority="high"
         layout="responsive"
         loading="eager"
-        src={pc_banner}
-        alt="Banner"
+        src={genesi}
+        alt="GENESI[IT]"
       />
       <SignUpForm />
     </div>
