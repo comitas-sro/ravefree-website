0bcc94082b70d30584b70d6c20b6fb7119bce77e
Samuel Mitas <samomitas@gmail.com>
2025-08-12 18:06:01 +0200

    Revert "Update wording"
    This reverts commit 3af357f7fe08478d5fb1d8391c4f5e3556c46976.

--- src/app/components/common/SignUpForm.tsx
+++ src/app/components/common/SignUpForm.tsx
@@ -8,7 +8,7 @@ after:absolute after:rounded-[calc(1rem-1px)] after:bg-base-100/30 dark:after:bg
       >
         <div className="relative z-10 p-1">
           <div className="bg-base-100 dark:bg-base-800 flex flex-col gap-5 rounded-xl px-4 py-10">
-            <h1 className="h2 text-center">SIGN UP FOR FREE ENTRY</h1>
+            <h1 className="h2 text-center">Registrácia pre vstup zdarma</h1>
             <iframe
               className="w-full min-h-[280px] mx-auto flex max-w-sm flex-col"
               loading="lazy"
