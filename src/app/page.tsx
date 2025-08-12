9c6332572fafe4b61d09e8628fdffaaf15cb35b6
Samuel Mitas <samomitas@gmail.com>
2025-08-12 21:15:21 +0200

    Rework banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import Image from 'next-export-optimize-images/image';
 
-import mobile_banner from './mobile-banner.webp';
-import pc_banner from './pc-banner.webp';
+import banner from './banner.webp';
+import genesis from './genesis.webp';
 import { SignUpForm } from './components/common/SignUpForm';
 
 export const metadata: Metadata = {
@@ -11,22 +11,31 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className='bg-linear-to-t from-[#20003A] to-zink-900 p-20'>
+    <div className="bg-linear-to-t from-[#20003A] to-black py-20">
       <Image
-        className='md:hidden'
+        className="max-w-sm m-auto px-14 z-10 relative"
         fetchPriority="high"
         layout="responsive"
         loading="eager"
-        src={mobile_banner}
+        src={banner}
         alt="Banner"
       />
+      <video
+        autoPlay
+        loop
+        muted
+        width="600"
+        className="m-auto -mt-10 md:-mt-20"
+      >
+        <source src="/banner.webm" type="video/webm" />
+      </video>
       <Image
-        className='hidden md:block'
+        className='max-w-sm m-auto -mt-20 md:-mt-30 px-16 z-10 relative'
         fetchPriority="high"
         layout="responsive"
         loading="eager"
-        src={pc_banner}
-        alt="Banner"
+        src={genesis}
+        alt="Genesis"
       />
       <SignUpForm />
     </div>
