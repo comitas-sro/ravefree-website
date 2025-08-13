4a25c436cb4660e05a05642a0743dafd701d983f
Samuel Mitas <samomitas@gmail.com>
2025-08-13 18:09:42 +0200

    Use new form

--- src/app/components/common/SignUpForm.tsx
+++ src/app/components/common/SignUpForm.tsx
@@ -8,9 +8,9 @@ after:absolute after:rounded-[calc(1rem-1px)] after:bg-base-800/50 after:inset-[
         <div className="relative z-10 p-1">
           <div className="bg-base-800 flex flex-col gap-5 rounded-xl px-4 py-10">
             <iframe
-              className="w-full min-h-[280px] mx-auto flex max-w-sm flex-col"
+              className="w-full min-h-[700px] mx-auto flex max-w-sm flex-col"
               loading="lazy"
-              src="https://tally.so/embed/mV686M?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+              src="https://tally.so/embed/3qljk9?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1&formEventsForwarding=1"
               frameBorder="0"
               marginWidth={0}
               marginHeight={0}
