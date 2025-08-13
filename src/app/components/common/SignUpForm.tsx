d0ef07bc533412bd7c2bfe67a97be61456d02e80
Samuel Mitas <samomitas@gmail.com>
2025-08-13 09:07:04 +0200

    Force dark mode

--- src/app/components/common/SignUpForm.tsx
+++ src/app/components/common/SignUpForm.tsx
@@ -2,12 +2,11 @@ export function SignUpForm() {
   return (
     <div className="site-container flex justify-center mt-16">
       <div
-        className="group max-w-xl relative h-full w-full rounded-2xl bg-gradient-to-br from-primary-400 via-base-300/70 to-base-300/70
-dark:from-primary-500 dark:via-base-300/50 dark:to-base-300/50
-after:absolute after:rounded-[calc(1rem-1px)] after:bg-base-100/30 dark:after:bg-base-800/50 after:inset-[1px]"
+        className="group max-w-xl relative h-full w-full rounded-2xl bg-gradient-to-br from-primary-500 via-base-300/50 to-base-300/50
+after:absolute after:rounded-[calc(1rem-1px)] after:bg-base-800/50 after:inset-[1px]"
       >
         <div className="relative z-10 p-1">
-          <div className="bg-base-100 dark:bg-base-800 flex flex-col gap-5 rounded-xl px-4 py-10">
+          <div className="bg-base-800 flex flex-col gap-5 rounded-xl px-4 py-10">
             <iframe
               className="w-full min-h-[280px] mx-auto flex max-w-sm flex-col"
               loading="lazy"
