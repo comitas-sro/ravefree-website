a9866677765ce7701ffbe9e18d113ee517047611
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2025-08-18 22:33:35 +0200

    Fix *.svg TypeScript types

--- svgr.d.ts
+++ svgr.d.ts
@@ -1,11 +1,11 @@
-import { FC, SVGProps } from "react";
-
 declare module "*.svg" {
+  import { FC, SVGProps } from "react";
   const content: FC<SVGProps<SVGElement>>;
   export default content;
 }
 
 declare module "*.svg?url" {
-  const content: any;
+  import { StaticImageData } from "next/dist/shared/lib/image-external";
+  const content: StaticImageData;
   export default content;
 }
