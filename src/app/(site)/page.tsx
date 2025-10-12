d63c895a461e5cc050b57ba35c7d1ef6b4f52c66
Samuel Mitas <smitas@powerfulmedical.com>
2025-10-12 17:49:09 +0200

    Preload poster with high priority

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,6 +6,7 @@ import video_banner from "./video-banner.webm";
 import banner from "./banner.webp";
 import genesi from "./genesi.webp";
 import { SignUpForm } from "../components/common/SignUpForm";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -13,50 +14,63 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="bg-linear-to-t from-[#20003A] to-black py-20">
-      <Image
-        className="max-w-sm m-auto px-14"
-        fetchPriority="high"
-        layout="responsive"
-        loading="eager"
-        src={banner}
-        alt="RAVE FREE"
-      />
-      <video
-        autoPlay
-        playsInline // Required for iOS to play without user interaction
-        loop
-        muted
-        width={600}
-        height={600}
-        className="m-auto"
-        poster={video_banner_fallback.src}
-      >
-        <source src={video_banner} type="video/webm" />
+    <>
+      <Head>
+        <link
+          rel="preload"
+          fetchPriority="high"
+          as="image"
+          href={video_banner_fallback.src}
+          type="image/webp"
+        />
+      </Head>
+      <div className="bg-linear-to-t from-[#20003A] to-black py-20">
         <Image
           className="max-w-sm m-auto px-14"
           fetchPriority="high"
           layout="responsive"
           loading="eager"
-          src={video_banner_fallback}
-          alt="RF"
+          src={banner}
+          alt="RAVE FREE"
         />
-      </video>
-      <Image
-        className="max-w-sm m-auto px-16"
-        fetchPriority="high"
-        layout="responsive"
-        loading="eager"
-        src={genesi}
-        alt="GENESI[IT]"
-      />
-      <div className="max-w-sm m-auto text-center">
-        <p className="text-[24px] leading-none mt-2">06/12/25</p>
-        <p className="text-[39px] leading-none">A4 STUDIO</p>
-        <p className="text-[18px] leading-none">BRATISLAVA SLOVAKIA</p>
-        <p className="text-[24px] leading-none mt-2">SIGN UP FOR FREE ENTRY</p>
+        <video
+          autoPlay
+          playsInline // Required for iOS to play without user interaction
+          loop
+          muted
+          width={600}
+          height={600}
+          className="m-auto"
+          poster={video_banner_fallback.src}
+        >
+          <source src={video_banner} type="video/webm" />
+          <Image
+            className="max-w-sm m-auto px-14"
+            fetchPriority="high"
+            layout="responsive"
+            loading="eager"
+            src={video_banner_fallback}
+            alt="RF"
+          />
+        </video>
+        <Image
+          className="max-w-sm m-auto px-16"
+          fetchPriority="high"
+          layout="responsive"
+          loading="eager"
+          src={genesi}
+          alt="GENESI[IT]"
+        />
+        <div className="max-w-sm m-auto text-center">
+          <p className="text-[24px] leading-none mt-2">06/12/25</p>
+          <p className="text-[39px] leading-none">A4 STUDIO</p>
+          <p className="text-[18px] leading-none">BRATISLAVA SLOVAKIA</p>
+          <p className="text-[24px] leading-none mt-2">
+            SIGN UP FOR FREE ENTRY
+          </p>
+        </div>
+        <SignUpForm />
       </div>
-      <SignUpForm />
-    </div>
+    </>
   );
 }
