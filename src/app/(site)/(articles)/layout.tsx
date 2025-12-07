c34fc0cbf3cdc1bbfc46417f94005594f1ad7fb6
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2025-12-07 21:48:58 +0100

    Upgrade Next.js to version 16
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/app/(site)/(articles)/layout.tsx
+++ src/app/(site)/(articles)/layout.tsx
@@ -6,7 +6,14 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <article className="mx-auto my-24 max-w-3xl px-4 md:mt-36 md:px-8 markdown-content">
+    // On Next.js 16, the pages don't scroll to the very top when smoothing
+    // scrolling is enabled. scroll-mt-20 is used in TableOfContents component
+    // of Astro Galaxy theme v6.2.2 to prevent the content from being hidden
+    // behind the header.
+    <article
+      className="mx-auto my-24 max-w-3xl px-4 md:mt-36 md:px-8 markdown-content
+      [&_*]:scroll-mt-20"
+    >
       {children}
     </article>
   );
